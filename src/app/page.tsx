1e7faa84534c60335434e0f8a85f1c75a726df51
Andrey Smirnoff <smirnoffa675@gmail.com>
2025-04-01 11:20:23 +0200

    started portfolio block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
-import NavBar from "@/components/NavBar/NavBar";
-import ShortDescription from "@/components/ShortDescription/ShortDescription";
+import NavBar from "@/components/MainPage/NavBar/NavBar";
+import PortFolio from "@/components/MainPage/Portfolio/Portfolio";
+import ShortDescription from "@/components/MainPage/ShortDescription/ShortDescription";
 
 export default function Home() {
   return (
     <>
       <NavBar/>
       <ShortDescription/>
+      <PortFolio/>
     </>
 
   )
---
+++ src/components/MainPage/Portfolio/Portfolio.tsx
@@ -0,0 +1,12 @@
+import styles from './portfolio.module.scss'
+
+export default function PortFolio(){
+    return (
+        <section className={styles.wrapper}>
+            <section className={styles.porfolio_header_wrapper}>
+                <h2 className={styles.portfolio_header}>Portfolio</h2>
+            </section>
+
+        </section>
+    )
+}
